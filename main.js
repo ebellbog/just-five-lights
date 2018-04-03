912182fdf8adb4c5303a56175ddfaecd3e51369a
Elana Bogdan <e.bell.bogdan@gmail.com>
2018-04-03 18:38:10 -0400

    add controls for mode and hue settings

--- main.js
+++ main.js
@@ -4,4 +4,33 @@ $(document).ready(function() {
     $light.addClass('light');
     $('#lights').append($light);
   }
+
+  $('#mode-hue').click(function() {
+    if ($(this).hasClass('selected')) return;
+
+    $(this).addClass('selected');
+    $('#mode-pc').removeClass('selected');
+
+    $('#hue-settings').css({display:'flex'});
+    setTimeout(()=>$('#hue-settings').animate({opacity:1},200), 300);
+    setTimeout(()=>
+      $('#mode-toggle, #mode-label').animate({'bottom':'+=110'}),
+      100);
+  });
+
+  $('#mode-pc').click(function() {
+    if ($(this).hasClass('selected')) return;
+
+    $(this).addClass('selected');
+    $('#mode-hue').removeClass('selected');
+
+    $('#hue-settings').animate({opacity:0},200);
+    setTimeout(()=>
+      $('#mode-toggle, #mode-label').animate(
+        {'bottom':'-=110'},
+        ()=>$('#hue-settings').css({display:'none'})
+      ),
+      100);
+  });
 });
+
